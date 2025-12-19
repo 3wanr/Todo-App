6e1e1ff9656f6c11a4811bc5b1fb9af464764f84
ejr7092 <ejr7092@rit.edu>
2025-12-19 10:57:19 -0500

    framework created

---
+++ server/api/db_utils.py
@@ -0,0 +1,34 @@
+import psycopg2
+import yaml
+import os
+
+def connect():
+    config = {}
+    yml_path = os.path.join(os.path.dirname(__file__), '../config/db.yml')
+
+    with open(yml_path, 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+
+    return psycopg2.connect(
+        dbname=config['database'],
+        user=config['user'],
+        password=config['password'],
+        host=config['host'],
+        port=config['port']
+    )
+
+def exec_get_one(sql, args={}):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(sql, args)
+    one = cur.fetchone()
+    conn.close()
+    return one
+
+def exec_commit(sql, args={}):
+    conn = connect()
+    cur = conn.cursor()
+    result = cur.execute(sql, args)
+    conn.commit()
+    conn.close()
+    return result
